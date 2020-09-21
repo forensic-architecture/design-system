f7b57d8c7cadaeab0ceea120888f7098c307052d
Zac Ioannidis <zacoppotamus@gmail.com>
2020-09-21 23:13:43 +0300

    Integrated react-three-fiber and drei into the sandbox

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,23 @@
 module.exports = {
-  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: [
+    "./**/*.stories.js",
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)",
+  ],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/preset-create-react-app",
+    "@storybook/addon-storysource",
   ],
+  webpackFinal: (config) => {
+    config.module.rules.push({
+      test: /\.(glsl|vs|fs|vert|frag)$/,
+      exclude: /node_modules/,
+      use: ["raw-loader", "glslify-loader"],
+      // include: path.resolve(__dirname, "../"),
+    });
+
+    return config;
+  },
 };
