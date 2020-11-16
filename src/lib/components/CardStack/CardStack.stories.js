3f473d90b3b3c235e2e0a0c53923915bd4e32eaa
Zac Ioannidis <zacoppotamus@gmail.com>
2020-11-16 17:59:15 +0000

    Removed CardStack - for now

--- src/lib/components/CardStack/CardStack.stories.js
+++ src/lib/components/CardStack/CardStack.stories.js
@@ -5,13 +5,15 @@ import { CardStackProps } from "../../../test/CardStack";
 
 const Template = (args) => <CardStack {...args} />;
 
-export const CardStack1 = Template.bind({});
-CardStack1.args = {
-  ...CardStackProps,
-};
+// TODO Understand what CardStacks should be concerned with, and what API they should expose
 
-export default {
-  title: "FA/Components/Card Stack",
-  component: CardStack,
-  argTypes: {},
-};
+// export const CardStack1 = Template.bind({});
+// CardStack1.args = {
+//   ...CardStackProps,
+// };
+
+// export default {
+//   title: "FA/Components/Card Stack",
+//   component: CardStack,
+//   argTypes: {},
+// };
